42c8a30a507409585a6a353a2c4cfc8ab20830d0
niravgadhia <niravgadhia@gmail.com>
2017-09-22 15:50:09 -0400

    Inital Project Files

---
+++ src/BookTester.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author 
+ *
+ */
+public class BookTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	/*	
+		Book example = new Book("The Da Vinci Code");
+		System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
+		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+		example.rented();
+		System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+		example.returned();
+		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+	*/
+	}
+
+}
