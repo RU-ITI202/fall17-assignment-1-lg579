411be1c8ff4c430329b7810c9c8f8379bf32d7af
lg579 <32248818+lg579@users.noreply.github.com>
2017-10-06 12:01:22 -0400

    Update BookTester.java

--- src/BookTester.java
+++ src/BookTester.java
@@ -4,21 +4,25 @@
  Individual Assignment 1
  Description: Create a book class with the methods getTitle, isBorrowed, rented, and returned*/
 
+
 public class BookTester {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 	
-		Book example = new Book("The Da Vinci Code");
-		System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-		example.rented();
-		System.out.println("Borrowed? (should be true): " + example.isBorrowed());
-		example.returned();
-		System.out.println("Borrowed? (should be false): " + example.isBorrowed());
-	
+		Book text = new Book("The Da Vinci Code");
+		System.out.println("Title (should be The Da Vinci Code): " + text.getTitle());
+		System.out.println("Borrowed? (should be false): " + text.isBorrowed());
+		text.rented();
+		System.out.println("Borrowed? (should be true): " + text.isBorrowed());
+		text.returned();
+		System.out.println("Borrowed? (should be false): " + text.isBorrowed());
+		
+		
+		//additional testing call
+		text.rented();
+		System.out.println("Borrowed? (should be true): " + text.isBorrowed());
 	}
 
 }
+
