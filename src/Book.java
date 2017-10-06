83c168c4235d0e9e68e4b3d8209fa02c2bce2670
lg579 <32248818+lg579@users.noreply.github.com>
2017-10-06 11:47:12 -0400

    Add files via upload

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+
+public class Book {
+
+	private String title;
+	private boolean borrow;
+	
+	
+	//constructor of book class
+	public Book() {
+		title= "";
+	}
+	
+	//sets title of book
+	public Book(String name) {
+		title = name;
+		
+	}
+	
+	//gets title of book
+	public String getTitle() {
+		return title;
+		
+	}
+	//returns boolean borrow value
+	public boolean isBorrowed() {
+		return borrow;
+		
+	}
+	
+	//sets borrow value to true if rented method is called
+	public void rented() {
+		borrow = true;
+	}
+	
+	//sets borrow value to false if returned method is called
+	public void returned() {
+		borrow= false;
+		
+	}
+	
+
+}
